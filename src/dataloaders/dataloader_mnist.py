1 error of BN fixed : Not scaling the images of Random batch 2 mnist_new1.py: deeper layers. 3 train_mnist.py: BN train and test variables only in train_mnist.py

src/dataloaders/dataloader_mnist.py
@@ -31,13 +31,6 @@ class MNISTDataLoader(object):
     def shuffle(self, dataset):
         np.random.shuffle(dataset)
 
-    def random_batch(self, split, batch_size):
-        perm = np.random.permutation(len(self.data[split]))
-        shuffled_data = self.data[split][perm]
-        n_batches = len(shuffled_data) // batch_size
-        i_batch = np.random.randint(0, n_batches)
-        return shuffled_data[i_batch * batch_size:(i_batch + 1) * batch_size]
-
     def _scale(self, x, feature_range=(-1, 1)):
         """
         Scale the images to have pixel values between -1 and 1
@@ -46,6 +39,14 @@ class MNISTDataLoader(object):
         min, max = feature_range
         x = x * (max - min) + min
         return x
+
+    def random_batch(self, split, batch_size):
+        perm = np.random.permutation(len(self.data[split]))
+        shuffled_data = self.data[split][perm]
+        n_batches = len(shuffled_data) // batch_size
+        i_batch = np.random.randint(0, n_batches)
+        batch = shuffled_data[i_batch * batch_size:(i_batch + 1) * batch_size]
+        return self._scale(batch)
 
     def next_batch(self, split):
         """

src/model_components/mnist.py
@@ -18,24 +18,25 @@ def n_layers_fc(inputs, n_units, activations=None, name='n_layers_fully_connecte
     return next_layer
 
 
-def transpose_conv2d(x, filters):
+def transpose_conv2d(x, filters, kernel_size=5, strides=2, padding='same'):
     kernel_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.02)
-    return tf.layers.conv2d_transpose(x, filters, 5, strides=2, padding='same', kernel_initializer=kernel_initializer)
+    return tf.layers.conv2d_transpose(x, filters, kernel_size=kernel_size, strides=strides, padding=padding,
+                                      kernel_initializer=kernel_initializer)
 
 
 def batch_norm(x, training, epsilon=1e-5, momentum=0.9):
     return tf.layers.batch_normalization(x, training=training, epsilon=epsilon, momentum=momentum)
+    # return x
 
-
-def dense(x, out_units):
+def dense(x, out_units, activation_fn=None):
     kernel = tf.random_normal_initializer(mean=0.0, stddev=0.3)
     #     return tf.layers.dense(x, out_units, activation=None)
-    return layers.fully_connected(x, out_units, activation_fn=None)
+    return layers.fully_connected(x, out_units, activation_fn=activation_fn)
 
 
-def conv2d(x, filters, name):
+def conv2d(x, filters, name, kernel_size=5, strides=2, padding="same", activation=None):
     kernel_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.02)
-    return tf.layers.conv2d(x, filters, kernel_size=5, strides=2, padding="same", activation=None,
+    return tf.layers.conv2d(x, filters, kernel_size=kernel_size, strides=strides, padding=padding, activation=activation,
                             kernel_initializer=kernel_initializer, name=name)
 
 

src/model_components/mnist_new1.py
@@ -19,10 +19,6 @@ def n_layers_fc(inputs, n_units, activations=None, name='n_layers_fully_connecte
     return next_layer
 
 
-def lrelu(x, alpha=0.2):
-    return tf.maximum(x, alpha * x)
-
-
 def transpose_conv2d(x, filters, kernel_size=5, strides=2, padding='same'):
     kernel_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.02)
     return tf.layers.conv2d_transpose(x, filters, kernel_size=kernel_size, strides=strides, padding=padding,
@@ -30,8 +26,9 @@ def transpose_conv2d(x, filters, kernel_size=5, strides=2, padding='same'):
 
 
 def batch_norm(x, training, epsilon=1e-5, momentum=0.9):
+    training = True
     return tf.layers.batch_normalization(x, training=training, epsilon=epsilon, momentum=momentum)
-
+    # return x
 
 def dense(x, out_units, activation_fn=None):
     kernel = tf.random_normal_initializer(mean=0.0, stddev=0.3)

src/models/dcgan.py
@@ -55,7 +55,7 @@ class Model(BaseModel):
         self.ph_img_recon = tf.placeholder(tf.float32, [None, None, None, 1])
         self.ph_img_gen = tf.placeholder(tf.float32, [None, None, None, 1])
         self.ph_training_flag = tf.placeholder(tf.bool)
-        self.test_bn = True
+        # self.test_bn = False
 
     def _define_network_graph(self):
         with tf.variable_scope(self.model_scope, reuse=tf.AUTO_REUSE):
@@ -198,20 +198,21 @@ class Model(BaseModel):
 
     def _define_operations(self):
         with tf.variable_scope(self.model_scope, reuse=tf.AUTO_REUSE):
+            autoencoder_trainable_params = tf.global_variables(self.encoder_scope) + tf.global_variables(self.decoder_scope)
+            opt1 = tf.train.AdamOptimizer(H.lr_autoencoder, beta1=H.beta1, beta2=H.beta2)
+
+            decoder_params = tf.global_variables(self.decoder_scope)
+            opt2 = tf.train.AdamOptimizer(H.lr_autoencoder, beta1=H.beta1, beta2=H.beta2)
+
+            disc_params = tf.global_variables(self.disc_scope)
+            opt3 = tf.train.AdamOptimizer(H.lr_autoencoder, beta1=H.beta1, beta2=H.beta2)
+
             extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-            # print('EXTRA_UPDATE_ops', extra_update_ops)
+
             with tf.control_dependencies(extra_update_ops):
-                autoencoder_trainable_params = tf.global_variables(self.encoder_scope) + tf.global_variables(self.decoder_scope)
-                opt = tf.train.AdamOptimizer(H.lr_autoencoder, beta1=H.beta1, beta2=H.beta2)
-                self.autoencoder_train_op = opt.minimize(self.encoder_loss, var_list=autoencoder_trainable_params)
-
-                decoder_params = tf.global_variables(self.decoder_scope)
-                opt = tf.train.AdamOptimizer(H.lr_autoencoder, beta1=H.beta1, beta2=H.beta2)
-                self.adv_gen_train_op = opt.minimize(self.gen_loss, var_list=decoder_params)
-
-                disc_params = tf.global_variables(self.disc_scope)
-                opt = tf.train.AdamOptimizer(H.lr_autoencoder, beta1=H.beta1, beta2=H.beta2)
-                self.adv_disc_train_op = opt.minimize(self.disc_loss, var_list=disc_params)
+                self.autoencoder_train_op = opt1.minimize(self.encoder_loss, var_list=autoencoder_trainable_params)
+                self.adv_gen_train_op = opt2.minimize(self.gen_loss, var_list=decoder_params)
+                self.adv_disc_train_op = opt3.minimize(self.disc_loss, var_list=disc_params)
 
     @property
     def network_loss_variables(self):
@@ -261,34 +262,34 @@ class Model(BaseModel):
     def run(self, fetches, feed_dict=None, options=None, run_metadata=None):
         return self.session.run(fetches, feed_dict, options, run_metadata)
 
-    def encode(self, x_batch):
+    def encode(self, x_batch, *args, **kwargs):
         return self.session.run(self.z_real, {
             self.ph_X: x_batch,
-            self.ph_training_flag: self.test_bn
+            self.ph_training_flag: kwargs['flag']
         })
 
-    def decode(self, z_batch):
+    def decode(self, z_batch, *args, **kwargs):
         return self.session.run(self.x_fake, {
             self.ph_Z: z_batch,
-            self.ph_training_flag: self.test_bn
+            self.ph_training_flag: kwargs['flag']
         })
 
-    def reconstruct_x(self, x_batch):
+    def reconstruct_x(self, x_batch, *args, **kwargs):
         return self.session.run(self.x_recon, {
             self.ph_X: x_batch,
-            self.ph_training_flag: self.test_bn
+            self.ph_training_flag: kwargs['flag']
         })
 
-    def reconstruct_z(self, z_batch):
+    def reconstruct_z(self, z_batch, *args, **kwargs):
         return self.session.run(self.z_recon, {
             self.ph_Z: z_batch,
-            self.ph_training_flag: self.test_bn
+            self.ph_training_flag: kwargs['flag']
         })
 
-    def discriminate(self, x_batch, split=True):
+    def discriminate(self, x_batch, split=True, *args, **kwargs):
         preds = self.session.run(self.disc_real_preds, {
             self.ph_X: x_batch,
-            self.ph_training_flag: self.test_bn
+            self.ph_training_flag: kwargs['flag']
         })[:, 0]
         if split:
             x_batch_real = x_batch[np.where(preds == 1)]

src/train_mnist.py
@@ -94,13 +94,13 @@ if 'all' in args.delete or 'weights' in args.delete:
 
 print('Train Test Data loaded...')
 
-max_epochs = 300000
+max_epochs = 100000
 
 n_step_console_log = 20
 n_step_tboard_log = 10
 n_step_validation = 50
 n_step_iter_save = 2000
-n_step_visualize = 200
+n_step_visualize = 100
 # n_step_generator_decay = 1500
 
 en_loss_history = []
@@ -111,16 +111,19 @@ g_acc_history = []
 gen_loss_history = []
 
 z_seed = {
-    'train': dl.get_z_dist(10, dim=H.z_size, dist_type=H.z_dist_type),
-    'test': dl.get_z_dist(10, dim=H.z_size, dist_type=H.z_dist_type)
+    'train': dl.get_z_dist(3, dim=H.z_size, dist_type=H.z_dist_type),
+    'test': dl.get_z_dist(3, dim=H.z_size, dist_type=H.z_dist_type)
 }
 x_seed = {
-    'train': dl.random_batch(split='train', batch_size=20),
-    'test': dl.random_batch(split='test', batch_size=20)
+    'train': dl.random_batch(split='train', batch_size=3),
+    'test': dl.random_batch(split='test', batch_size=3)
 }
+# x_seed['test'] = x_seed['train']
 
 BN_train = True
-BN_test = True
+BN_test = False
+print('BN_train: ',BN_train)
+print('BN_test: ',BN_test)
 
 while iter_no < max_epochs:
     iter_no += 1
@@ -129,8 +132,12 @@ while iter_no < max_epochs:
     # print('x_train',x_train.shape)
     # print('x_test',x_test.shape)
     x_train, x_test = dl.get_data()
+    # x_test = x_train
+
     z_train = dl.get_z_dist(x_train.shape[0], dim=H.z_size, dist_type=H.z_dist_type)
     z_test = dl.get_z_dist(x_test.shape[0], dim=H.z_size, dist_type=H.z_dist_type)
+
+    # z_test = z_train
 
     # print('z_train', z_train.shape)
     # print('z_test', z_test.shape)
@@ -138,12 +145,9 @@ while iter_no < max_epochs:
     train_inputs = x_train, z_train, BN_train
     test_inputs = x_test, z_test, BN_test
 
-    if H.train_autoencoder:
-        model.step_train_autoencoder(train_inputs)
-
     if (iter_no % H.combined_iter_count) < H.gen_iter_count:
-        if H.train_generator_adv:
-            model.step_train_adv_generator(train_inputs)
+        model.step_train_encoder(train_inputs)
+        model.step_train_decoder(train_inputs)
     else:
         model.step_train_discriminator(train_inputs)
 
@@ -153,11 +157,11 @@ while iter_no < max_epochs:
     train_losses = model.compute_losses(train_inputs, model.network_loss_variables)
     #
     if iter_no % n_step_console_log == 0:
-        print('Step %i: Encoder Loss: %f' % (iter_no, train_losses[0]))
-        print('Step %i: Disc Acc: %f' % (iter_no, train_losses[1]))
-        print('Step %i: Gen  Acc: %f' % (iter_no, train_losses[2]))
-        print('Step %i: x_recon Loss: %f' % (iter_no, train_losses[3]))
-        print('Step %i: z_recon Loss: %f' % (iter_no, train_losses[4]))
+        print('%s: Step %i: Encoder Loss: %f' % (ExperimentContext.exp_name, iter_no, train_losses[0]))
+        print('%s: Step %i: Disc Acc: %f' % (ExperimentContext.exp_name, iter_no, train_losses[1]))
+        print('%s: Step %i: Gen  Acc: %f' % (ExperimentContext.exp_name, iter_no, train_losses[2]))
+        print('%s: Step %i: x_recon Loss: %f' % (ExperimentContext.exp_name, iter_no, train_losses[3]))
+        print('%s: Step %i: z_recon Loss: %f' % (ExperimentContext.exp_name, iter_no, train_losses[4]))
         print()
 
     if iter_no % n_step_tboard_log == 0:
@@ -166,9 +170,12 @@ while iter_no < max_epochs:
     if iter_no % n_step_visualize == 0:
         for split in ['train', 'test']:
             x_gt = x_seed[split]
-            x_recon_seed = model.reconstruct_x(x_seed[split])
-            x_gen_seed = model.decode(z_seed[split])
-
+            bn_flag = BN_train if split == 'train' else BN_test
+            x_recon_seed = model.reconstruct_x(x_seed[split], flag=bn_flag)
+            print('loss', np.mean((x_recon_seed - x_seed[split]) ** 2))
+            x_gen_seed = model.decode(z_seed[split], flag=bn_flag)
+            # x_recon_seed = model.reconstruct_x(x_seed[split])
+            # x_gen_seed = model.decode(z_seed[split])
             model.log_images(split, [x_gt, x_recon_seed, x_gen_seed], iter_no)
 
     if iter_no % n_step_validation == 0:
@@ -181,4 +188,4 @@ while iter_no < max_epochs:
     iter_time_end = time.time()
 
     if iter_no % n_step_console_log == 0:
-        print('Single Iter Time: %.4f' % (iter_time_end - iter_time_start))
+        print('%s: Single Iter Time: %.4f' % (ExperimentContext.exp_name, iter_time_end - iter_time_start))