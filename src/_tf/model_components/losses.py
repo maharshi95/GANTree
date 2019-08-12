466438e9c46dc27fa50e4dcc7f0c42d957309c1f
dakshitagrawal97 <dagrawal@cs.iitr.ac.in>
2019-08-12 13:02:32 +0530

    remove tf and test files

--- src/_tf/model_components/losses.py
+++
@@ -1,8 +0,0 @@
-from __future__ import division
-import tensorflow as tf
-
-
-def sigmoid_cross_entropy_loss(labels, logits, name=None):
-    return tf.reduce_mean(
-        tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits, name=name)
-    )
